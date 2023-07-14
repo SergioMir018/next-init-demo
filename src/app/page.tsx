32eed6bda1b758b5cf56066ff90f42bcd3f51aef
Serio_Mir <voidtracer6@gmail.com>
2023-07-14 13:04:21 -0400

    MOD
    modifications on the <head> of the pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
 import Image from 'next/image';
+import { Metadata } from "next";
 
-// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Home page of the demo Next app',
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -13,7 +18,7 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <Link href="/about">
             <div
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              className="pointer-events-none flex text-2xl place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
               rel="noopener noreferrer"
             >
               About
